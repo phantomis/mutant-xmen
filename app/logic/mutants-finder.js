8f498ef7bebf88136a6d7411f127581d04df2701
Rodrigo Amaro <rodrigo.amaro.r@gmail.com>
2019-08-13 00:07:13 -0400

    Fixing inline comments to improve readability

--- app/logic/mutants-finder.js
+++ app/logic/mutants-finder.js
@@ -1,7 +1,16 @@
 class Mutants {
 
+    /**
+     * Strategy: iterate over every i,j item on the dna matrix, and check arround it the 
+     * bottom left, bottom, bottom right and right value to check the equals values, if it's equal,
+     * carries the counter of found items to the next i+1,j element, until we reach the counterLimit of (4)
+     * equals GENS, reseting the counters and decreasing the tries.
+     * 
+     * Complexity of O(n^2) , but using memory to save precalculated items 
+     */
     constructor(){
         this.tries = 2;
+        this.mutantGenCounterLimit = 4;  
     }
     isMutant(dna){
         if (!Array.isArray(dna) ){
@@ -11,28 +20,29 @@ class Mutants {
         for(let i = 0; i < dna.length; i++ ){
             let line = dna[i].split('');
             for (let j = 0; j < line.length; j++){
-                let centerNumber = dna[i].charAt(j);
+                let centerGen = dna[i].charAt(j);
                 
+                // Check if DiagonalLeft item is equal to current item
                 if(i+1 < dna.length && j-1 >= 0){
-                    if(dna[i+1].charAt(j-1) == centerNumber){
+                    if(dna[i+1].charAt(j-1) == centerGen){
                         if(this.incrementAndCheckLimits(i+1, j-1,this.carryCounterGens[i][j], "dl")) return true;
                     }
                 }
-    
+                // Check if Vertical item is equal to current item
                 if(i+1 < dna.length && j >= 0){
-                    if(dna[i+1].charAt(j) == centerNumber){
+                    if(dna[i+1].charAt(j) == centerGen){
                         if(this.incrementAndCheckLimits(i+1, j, this.carryCounterGens[i][j], "v")) return true;
                     }
                 }
-    
+                // Check if DiagonalRight item is equal to current item
                 if(i+1 < dna.length && j+1 < dna[i].length){
-                    if(dna[i+1].charAt(j+1) == centerNumber){
+                    if(dna[i+1].charAt(j+1) == centerGen){
                         if(this.incrementAndCheckLimits(i+1, j+1,this.carryCounterGens[i][j], "dr")) return true;
                     }
                 }
-    
+                // Check if Horizontal item is equal to current item
                 if(j+1 < dna[i].length){
-                    if(dna[i].charAt(j+1) == centerNumber){
+                    if(dna[i].charAt(j+1) == centerGen){
                         if(this.incrementAndCheckLimits(i, j+1,this.carryCounterGens[i][j], "h")) return true;
                     }
                 }
@@ -40,7 +50,13 @@ class Mutants {
         }
         return false;
     }
-
+    /*
+    * Create and empty array with a structure, to carry the counter of currents values found.
+    * h: number of horizontal values found
+    * v: number of vertical values found
+    * dl: Number of diagonal left items found ( / )
+    * dr: Number of diagonal right items found ( \ )
+    */
     createEmptyMatrix(dna){
         let array = [];
         for(let i=0;i<dna.length;i++){
@@ -52,9 +68,14 @@ class Mutants {
         return array;
     }
     
+    /*
+    * Increment the currents structure on the directon we tested, reset the counter if we 
+    * reach the mutantGenCounterLimit. 
+    * Checks if we pass the "try" limit with the current mutantGenCounterLimit, in that case we returns 
+    */
     incrementAndCheckLimits(i, j, currentValue, dir){
         this.carryCounterGens[i][j][dir] = this.carryCounterGens[i][j][dir] + 1 + currentValue[dir];
-        if (this.carryCounterGens[i][j][dir] >= 3){
+        if (this.carryCounterGens[i][j][dir] >= this.mutantGenCounterLimit-1){
             this.tries -= 1;
             this.carryCounterGens[i][j][dir] = 0;
             if(this.tries == 0){
@@ -64,6 +85,5 @@ class Mutants {
         return false;
     }
 
-    // incrementCarryCounter()
 }
 exports.Mutants = Mutants;
